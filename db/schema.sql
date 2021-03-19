66f9b081b168e7b59c6a3e8337618b497dbd1b07
Nate J <jamis.walsh@gmail.com>
2021-03-18 23:13:21 -0700

    small changes. commented out future features

--- db/schema.sql
+++ db/schema.sql
@@ -6,13 +6,13 @@ USE employees_db;
 
 CREATE TABLE department (
     id INT AUTO_INCREMENT NOT NULL,
-    name VARCHAR(255) NOT NULL,
+    name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
     id INT AUTO_INCREMENT NOT NULL,
-    title VARCHAR(255) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2),
     department_id INT NOT NULL,
     PRIMARY KEY(id),
@@ -24,8 +24,6 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT,
     PRIMARY KEY(id),
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
